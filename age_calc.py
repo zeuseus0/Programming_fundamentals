59f36e882b2acf0ad0c8d735245ef7b91a55e05e
zeuseus0 <munkombwe.emmanuel@gmail.com>
2025-09-10 23:32:47 +0200

    Add files via upload

---
+++ age_calc.py
@@ -0,0 +1,9 @@
+
+name = input("Enter your name: ")
+
+birth_year = int(input("Enter your birth year: "))
+
+current_year = 2025
+age = current_year - birth_year
+
+print(f"Hello {name}! You are {age} years old.")
